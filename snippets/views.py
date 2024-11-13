28005a78dcdb1ec6e1ac7a7a540f71524494df7d
mironova2056 <145144059+mironova2056@users.noreply.github.com>
2024-11-13 20:10:54 +0700

    fix

--- snippets/views.py
+++ snippets/views.py
@@ -5,7 +5,7 @@ from .serializers import SnippetSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
 from rest_framework.reverse import reverse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 
 
 @api_view(['GET'])
