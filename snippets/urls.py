8148a699f0d876c3d112e95dc0300655f161ed3d
mironova2056 <145144059+mironova2056@users.noreply.github.com>
2024-11-13 20:45:49 +0700

    some fix

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,47 +1,12 @@
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
-from .views import SnippetViewSet, UserViewSet
-from rest_framework import renderers, viewsets
-from . import views
 from rest_framework.routers import DefaultRouter
 
+from . import views
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-   path('', include(router.urls)),
-]
-
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
-
-urlpatterns = [
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
-    path('users/', user_list, name='user-list'),
-    path('users/<int:pk>/', user_detail, name='user-detail'),
+    path('', include(router.urls)),
     path('auth/', include('rest_framework.urls')),
-    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
-    path('', views.api_root, name='api-root'),  # добавлено name
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
