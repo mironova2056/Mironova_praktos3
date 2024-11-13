83fa547c5e2277e79316780b17788ee806425f29
mironova2056 <145144059+mironova2056@users.noreply.github.com>
2024-11-13 19:25:18 +0700

    association of snippets with users

--- snippets/views.py
+++ snippets/views.py
@@ -8,10 +8,14 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
 class UserList(generics.ListAPIView):
    queryset = User.objects.all()
    serializer_class = UserSerializer
