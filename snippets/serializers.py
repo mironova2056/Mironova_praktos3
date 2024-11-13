5d4b3a9b8a92dc994b19c66a4ab30ab7e82925ae
mironova2056 <145144059+mironova2056@users.noreply.github.com>
2024-11-13 20:01:15 +0700

    related objects

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -4,16 +4,18 @@ from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+        fields = ['url', 'id', 'highlight', 'owner',
+                 'title', 'code', 'linenos', 'language', 'style']
 class UserSerializer(serializers.ModelSerializer):
 
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all(), required=False)
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
 
     def create(self, validated_data):
         snippets_data = validated_data.pop('snippets', [])
