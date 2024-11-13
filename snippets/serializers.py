968f067973b5a1d9605c953d86ae2a0155330f19
mironova2056 <145144059+mironova2056@users.noreply.github.com>
2024-11-13 19:21:12 +0700

    add views for user

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from .models import Snippet
+from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+class UserSerializer(serializers.ModelSerializer):
+
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all(), required=False)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
+
+    def create(self, validated_data):
+        snippets_data = validated_data.pop('snippets', [])
+        user = User.objects.create(**validated_data)
+        user.snippets.set(snippets_data)
+        return user
--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .models import Snippet
-from .serializers import SnippetSerializer
+from django.contrib.auth.models import User
+from .serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -11,3 +12,11 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+class UserList(generics.ListAPIView):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
